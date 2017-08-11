31501a3e6ba3ba56f0d237713dcda30335a33475
unknown <hgl1002@naver.com>
2017-08-11 13:51:50 +0900

    Added BinaryTreeExporter.

---
+++ app/src/main/java/me/blog/hgl1002/lboard/expression/TreeExporter.java
@@ -0,0 +1,9 @@
+package me.blog.hgl1002.lboard.expression;
+
+import me.blog.hgl1002.lboard.expression.nodes.TreeNode;
+
+public interface TreeExporter {
+
+	public Object export(TreeNode node);
+
+}
