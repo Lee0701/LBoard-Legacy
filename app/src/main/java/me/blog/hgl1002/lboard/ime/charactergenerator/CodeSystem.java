1705464bd5c2eea488ffd835584f42b172e7d9a5
Lee0701 <hgl1002@naver.com>
2017-08-14 10:15:27 +0900

    Added comment.

--- app/src/main/java/me/blog/hgl1002/lboard/ime/charactergenerator/CodeSystem.java
+++ app/src/main/java/me/blog/hgl1002/lboard/ime/charactergenerator/CodeSystem.java
@@ -1,4 +1,12 @@
 package me.blog.hgl1002.lboard.ime.charactergenerator;
 
+/**
+ * CodeSystem.
+ *
+ * LBoard의 문자 생성기에서 사용하는 코드 시스템.
+ * 어떤 코드를 어떻게 인식할까 정의한다.
+ *
+ * @author Hyegyu Lee
+ */
 public interface CodeSystem {
 }
