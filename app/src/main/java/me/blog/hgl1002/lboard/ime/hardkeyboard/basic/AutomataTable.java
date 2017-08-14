e00486fdb59691098060a10cf96bd4ce61eebbff
Lee0701 <hgl1002@naver.com>
2017-08-14 09:14:14 +0900

    Added initial rule to AutomataTable.

--- app/src/main/java/me/blog/hgl1002/lboard/ime/hardkeyboard/basic/AutomataTable.java
+++ app/src/main/java/me/blog/hgl1002/lboard/ime/hardkeyboard/basic/AutomataTable.java
@@ -2,12 +2,21 @@ package me.blog.hgl1002.lboard.ime.hardkeyboard.basic;
 
 import java.util.Map;
 
+import me.blog.hgl1002.lboard.expression.nodes.ConstantTreeNode;
+
 public class AutomataTable {
 
 	protected Map<Long, AutomataRule> table;
 
 	public AutomataTable() {
+		AutomataRule initial = new AutomataRule();
+		initial.setInitialState(0);
+		initial.setTargetState(new ConstantTreeNode(0L));
+		table.put(0L, initial);
+	}
 
+	public AutomataTable(AutomataRule initial) {
+		table.put(0L, initial);
 	}
 
 	public AutomataRule get(long index) {
